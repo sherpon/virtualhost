12dd3ddb4c949148f399a59bec70e7474f0c2975
bruce grover lee <brucegrover.lee@gmail.com>
2019-06-03 09:09:17 -0500

    added virtualhostApp

--- src/virtualhostApp.js
+++ src/virtualhostApp.js
@@ -24,18 +24,45 @@ if (process.env.NODE_ENV !== 'test') {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'public'));
 
-// ********************************************
-// API
-// ********************************************
-app.use('/users', require(path.join(__dirname, 'api/users')) );
-app.use('/sites', require(path.join(__dirname, 'api/sites')) );
 const websiteCreatorRouter = require('./websiteCreator');
 const virtualhostServerRouter = require('./virtualhostServer');
 
-app.use(vhost(process.env.WEBSITE_CREATOR_ENDPOINT,  websiteCreatorRouter ));
+app.use(vhost(process.env.WEBSITE_CREATOR_ENDPOINT, websiteCreatorRouter));
+app.use(vhost(/[a-z]/ig, virtualhostServerRouter));
 
-console.log('DOTENV_PATH');
-console.log(process.env.DOTENV_PATH);
+// ********************************************
+// The 404 Route
+// ********************************************
+app.use(function(req, res, next){
+  res.status(404);
+  if (req.accepts('html')) {
+    // respond with html page
+    res.type('txt').send('Sherpon virtualhost app ' + process.env.NODE_ENV + ': Not found');
+  }
+  else if (req.accepts('json')) {
+    // respond with json
+    res.send({ error: 'Sherpon virtualhost app ' + process.env.NODE_ENV + ': Not found' });
+  } else {
+    // default to plain-text. send()
+    res.type('txt').send('Sherpon virtualhost app ' + process.env.NODE_ENV + ': Not found');
+  }
+})
 
-console.log('WEBSITE_CREATOR_ENDPOINT');
-console.log(process.env.WEBSITE_CREATOR_ENDPOINT);
+// console.log('DOTENV_PATH');
+// console.log(process.env.DOTENV_PATH);
+
+// console.log('WEBSITE_CREATOR_ENDPOINT');
+// console.log(process.env.WEBSITE_CREATOR_ENDPOINT);
+
+// DO NOT DO app.listen() unless we're testing this directly
+if (require.main === module) { 
+  //Start Sherpon Virtualhost
+  var port = process.env.PORT || 7000
+  app.listen(port, function () {
+    console.log(colors.blue('NODE_ENV: %s'), process.env.NODE_ENV)
+    console.log(colors.cyan('Sherpon Virtualhost running on http://*.localhost:%d/'), port)
+  })
+}
+
+// Instead do export the app:
+module.exports = app;
